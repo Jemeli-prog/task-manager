fb1722dbf3b64f5c7e3d47c39bf0cc71e1c27a58
Ruth Kiptoo <ruthkiptoo523@gmail.com>
2025-08-27 12:15:04 +0300

    updated admin

--- task_manager/duty/admin.py
+++ task_manager/duty/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Duty
+
+@admin.register(Duty)
+class DutyAdmin(admin.ModelAdmin):
+    list_display = ('title','is_done')
+    list_filter = ('is_done',)
+    search_fields = ('title', 'description')
 
 # Register your models here.
